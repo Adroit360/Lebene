b2f0ba99453e2824e5c878b754915274b8dd6ad0
Moro-Afriyie <ceremoinc99@gmail.com>
2022-05-31 22:13:52 +0000

    feat:update the website

--- restaurant-website/src/app/order-page/order-page.component.ts
+++ restaurant-website/src/app/order-page/order-page.component.ts
@@ -244,8 +244,8 @@ export class OrderPageComponent implements OnInit {
     //     }
     //   });
     const body = {
-      //amount: this.totalPrice * 100,
-      amount: 0.01 * 100,
+      amount: this.totalPrice * 100,
+      //amount: 0.03 * 100,
       clientId: this.clientTransactionId,
       orderDetails: this.orderDetails,
     };
